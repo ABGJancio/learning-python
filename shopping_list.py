5415a2872d0342aa37bb02e3722f8f20697f772f
ABGJancio <77855688+ABGJancio@users.noreply.github.com>
2021-01-22 21:09:21 +0100

    Add missed line

--- shopping_list.py
+++ shopping_list.py
@@ -7,6 +7,7 @@ q = []
 for shop, prod_list in shopping.items():
     shop = shop.capitalize()
     prod_list = [prod.capitalize() for prod in prod_list]
+    q.append(len(prod_list))
     print('Idę do', shop, end = ', ')
     print('kupuję tu następujące rzeczy:', prod_list, end = '.\n')
 print('W sumie kupuję', sum(q), 'produktów.\n')
