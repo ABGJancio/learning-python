543e0403f1c258f966cb17c298c902b48381729f
Jan Kondratiuk <abg.projekt@wp.pl>
2021-02-28 14:35:18 +0100

    Trochę poprawiłem

--- BCwithFaker.py
+++ BCwithFaker.py
@@ -1,12 +1,12 @@
 from faker import Faker
 fake = Faker('pl_PL')
-#def create_contacts(kind, q):
-#    if kind == "BaseContacts":
-#        new_contacts = [BaseContact(fake.first_name(), fake.last_name(), fake.company_email(), fake.phone_number()) for _ in range(q)]
-#    elif kind == "BusinessContacts":
-#        extended_contacts = [BusinessContact(fake.first_name(), fake.last_name(), fake.company_email(), fake.phone_number(), fake.company(), fake.job(), fake.phone_number()) for _ in range(q)]
-#    else:
-#        print("Set right kind!")
+def create_contacts(kind, q):
+    if kind == "BaseContacts":
+        return [BaseContact(fake.first_name(), fake.last_name(), fake.company_email(), fake.phone_number()) for _ in range(q)]
+    elif kind == "BusinessContacts":
+        return [BusinessContact(fake.first_name(), fake.last_name(), fake.company_email(), fake.phone_number(), fake.company(), fake.job(), fake.phone_number()) for _ in range(q)]
+    else:
+        return "Set right kind!"
 
 class BaseContact:
     def __init__(self, first_name, last_name, e_mail, phone):
@@ -16,24 +16,30 @@ class BaseContact:
         self.phone = phone
         #Variables
         self.label_max = 16
+
     def __str__(self):
         return f'{self.first_name} {self.last_name} : {self.phone}'
+
     def __repr__(self):
         return f"\nfirst_name={self.first_name} last_name={self.last_name}, e_mail={self.e_mail}, phone={self.phone}"
+
     def contact(self):
         return f"Wybieram numer {self.phone} i dzwonię do {self.first_name} {self.last_name}"
+
     @property
     def label_length(self):
         return len(f"{self.first_name} {self.last_name}")
+
+    #@label_length.setter
     @property
     def length_check(self):
         if self.label_length <= self.label_max:
-            print(f"Name {self.first_name} {self.last_name} fits in the address line")
+            return f"Name {self.first_name} {self.last_name} fits in the address line"
         else:
             try:
                 raise ValueError()
             except ValueError:
-                print(f"Name {self.first_name} {self.last_name} is to long - {self.label_length} characters; exceeds top length of address line: 15 characters")
+                return f"Name {self.first_name} {self.last_name} is to long - {self.label_length} characters; exceeds top length of address line: 15 characters"
 
 class BusinessContact(BaseContact):
     def __init__(self, first_name, last_name, e_mail, phone, company, occupation, business_phone):
@@ -41,30 +47,19 @@ class BusinessContact(BaseContact):
         self.company = company
         self.occupation = occupation
         self.business_phone = business_phone
+    
     def __str__(self):
         return f'{self.first_name} {self.last_name} : {self.business_phone}'
+    
     def contact(self):
         return f"wybieram numer {self.business_phone} i dzwonię do {self.first_name} {self.last_name}"
 
-#create_contacts("BaseContacts", 5)
-new_contacts = [BaseContact(fake.first_name(), fake.last_name(), fake.company_email(), fake.phone_number()) for _ in range(5)]
-nc_one = new_contacts[0]
-nc_two = new_contacts[1]
-nc_three = new_contacts[2]
-nc_four = new_contacts[3]
-nc_five = new_contacts[4]
+base_contacts = create_contacts("BaseContacts", 5)
 
-extended_contacts = [BusinessContact(fake.first_name(), fake.last_name(), fake.company_email(), 
-                                     fake.phone_number(), fake.company(), fake.job(), fake.phone_number()
-                                    ) for _ in range(5)]
-ec_one = extended_contacts[0]
-ec_two = extended_contacts[1]
-ec_three = extended_contacts[2]
-ec_four = extended_contacts[3]
-ec_five = extended_contacts[4]
+business_contacts = create_contacts("BusinessContacts", 5)
 
-print(nc_two.contact())
-print(ec_three.contact())
+print(base_contacts[0].contact())
+print(business_contacts[2].contact())
 
-print(nc_five.length_check)
-print(ec_four.length_check)
+print(base_contacts[4].length_check)
+print(business_contacts[3].length_check)
